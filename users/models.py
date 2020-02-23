afd39f7e318ed779e3cd199c3912871c3aac3ae2
pkrzyszowski <pkrzyszowski@gmail.com>
2020-02-23 23:30:21 +0100

    start project

---
+++ users/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Subscriber(models.Model):
+    pass
+
+
+class SubscriberSMS(models.Model):
+    pass
+
+
+class User(models.Model):
+    pass
+
+
+class Client(models.Model):
+    pass
+
+
+class User(models.Model):
+    pass
