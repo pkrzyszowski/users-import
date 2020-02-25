cdfca14e0bf66a278fd6c31698ab38b251432053
pkrzyszowski <pkrzyszowski@gmail.com>
2020-02-25 16:56:46 +0100

    models migrations; initial data for User

--- users/models.py
+++ users/models.py
@@ -2,20 +2,37 @@ from django.db import models
 
 
 class Subscriber(models.Model):
-    pass
+    create_date = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(unique=True)
+    gdpr_consent = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
 
 
 class SubscriberSMS(models.Model):
-    pass
+    create_date = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(unique=True, max_length=20)
+    gdpr_consent = models.BooleanField(default=False)
 
-
-class User(models.Model):
-    pass
+    def __str__(self):
+        return self.phone
 
 
 class Client(models.Model):
-    pass
+    create_date = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(unique=True, max_length=20)
+
+    def __str__(self):
+        return self.email
 
 
 class User(models.Model):
-    pass
+    create_date = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(unique=True, max_length=20)
+    gdpr_consent = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
